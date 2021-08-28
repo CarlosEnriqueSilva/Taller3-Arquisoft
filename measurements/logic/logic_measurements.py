4b31cca91a603af077c3dfdb80ca8c396a6f5389
Carlos Enrique Silva <69487125+CarlosEnriqueSilva@users.noreply.github.com>
2021-08-28 14:36:36 -0500

    Carpetas

---
+++ measurements/logic/logic_measurements.py
@@ -0,0 +1,6 @@
+from ..models import Measurement
+
+def get_all_measurements():
+    measurements = Measurement.objects.all()
+    return measurements
+    
---
+++ measurements/views.py
@@ -0,0 +1,8 @@
+from .logic.logic_measurements import get_all_measurements
+from django.http import HttpResponse
+from django.core import serializers
+
+def get_measurements(request):
+    measurements = get_all_measurements()
+    measurement_list = serializers.serialize('json', measurements)
+    return HttpResponse(measurement_list,content_type='application/json')
